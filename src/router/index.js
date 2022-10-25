6b875b38d75bc3426349351ada29d72a804ed2e2
Kurja <kurja0403@gmail.com>
2022-10-25 16:10:36 +0800

    路由配置

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+const router = createRouter({
+  history: createWebHashHistory(), // hash模式：createWebHashHistory，history模式：createWebHistory
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    }
+  ]
+})
+
+export default router
