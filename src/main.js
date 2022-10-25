b6e0d451d5b33e29f662fd6eab3f2a9ac63ad124
Kurja <kurja0403@gmail.com>
2022-10-25 15:15:12 +0800

    引入、全局过滤器

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+import vant from 'vant'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// 全局过滤器
+app.config.globalProperties.$filters = {
+  prefix (url) = {
+  if (url && url.startWith('http')) {
+    return url
+  } else {
+    url = `http://backend-api-01.newbee.ltd${url}`
+    return url
+  }
+}
+}
+
+app.use(vant)
